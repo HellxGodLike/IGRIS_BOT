d18b8999e9fe341bdbaef315b439e85923d847a9
HeLLxGodLike <ysonu4951@gmail.com>
2020-12-07 00:11:02 +0530

    Update anime.py

--- SaitamaRobot/modules/anime.py
+++ SaitamaRobot/modules/anime.py
@@ -174,7 +174,7 @@ def airing(update: Update, context: CallbackContext):
             'variables': variables
         }).json()['data']['Media']
     image = response.get('bannerImage', None)
-    msg = f"*Name*:[⁠ ⁠]({image})*{response['title']['romaji']}*(`{response['title']['native']}`)\n*ID*: `{response['id']}"
+    msg = f"*Name*: *{response[⁠ ⁠](['image']}*{response['title']['romaji']}*(`{response['title']['native']}`)\n*ID*: `{response['id']}"
     if response['nextAiringEpisode']:
         time = response['nextAiringEpisode']['timeUntilAiring'] * 1000
         time = t(time)
