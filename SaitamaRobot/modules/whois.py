882517d4c182e1d96717f9e6fdd8796f8a34fb51
HeLLxGodLike <ysonu4951@gmail.com>
2020-12-07 03:05:10 +0530

    Update whois.py

--- SaitamaRobot/modules/whois.py
+++ SaitamaRobot/modules/whois.py
@@ -53,7 +53,6 @@ def FullName(user: User):
     return user.first_name + " " + user.last_name if user.last_name else user.first_name
 
 
-@igris.on_message(filters.command("whois"))
 async def whois(client, message):
     cmd = message.command
     if not message.reply_to_message and len(cmd) == 1:
