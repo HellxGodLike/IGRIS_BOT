9de702366224a916f5ef45c83e43e47f841465cd
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-06 18:25:41 +0530

    Create VOICECHAT.py

---
+++ SaitamaRobot/modules/VOICECHAT.py
@@ -0,0 +1,15 @@
+__help__ = """
+ • /playm <reply to song u want to play in vc>:Add @IGRISMUSIC in group and play song u want to play in vc
+
+ • /stopm <to stop the song playing in vc> : Stop the song playing in vc
+
+ • /skip To skip to the next song 
+
+ • /ytp <similar to playm but don't need to reply the song> Play song from YouTube
+
+ **IMPORTANT REQUIREMENTS**
+
+ `Add @IGRISMUSIC in group before playing any song else this will not work'
+"""
+
+__mod_name__ = "VOICECHAT"
