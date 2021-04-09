bbfa43e94a44e6b07a9e38ab05c140965722b91c
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-10 01:48:36 +0530

    Update admin.py

--- SaitamaRobot/modules/admin.py
+++ SaitamaRobot/modules/admin.py
@@ -438,24 +438,30 @@ def adminlist(update, context):
 
 
 __help__ = """
- • `/admins`*:* list of admins in the chat
+ ✶ `/admins`*:* list of admins in the chat
 
 *Admins only:*
- • `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
- • `/unpin`*:* unpins the currently pinned message
- • `/invitelink`*:* gets invitelink
- • `/promote`*:* promotes the user replied to
- • `/demote`*:* demotes the user replied to
- • `/title <title here>`*:* sets a custom title for an admin that the bot promoted
+ ✶ `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
+ ✶ `/unpin`*:* unpins the currently pinned message
+ ✶ `/invitelink`*:* gets invitelink
+ ✶ `/promote`*:* promotes the user replied to
+ ✶ `/demote`*:* demotes the user replied to
+ ✶ `/title <title here>`*:* sets a custom title for an admin that the bot promoted
+ ✶  `/approval`*:* Check a user's approval status in this chat.
+ ✶ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+ ✶ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+ ✶ `/approved`*:* List all approved users.
+ ✶ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
  
+
  *Remote commands*
  
- •  `/rban user group`*:* Remote ban
- • `/runban user group`*:* Remote un-ban
- •  `/rpunch user group`*:* Remote punch
- •  `/rmute user group`*:* Remote mute
- •  `/runmute user group`*:* Remote un-mute
- •  `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ✶  `/rban user group`*:* Remote ban
+ ✶ `/runban user group`*:* Remote un-ban
+ ✶  `/rpunch user group`*:* Remote punch
+ ✶  `/rmute user group`*:* Remote mute
+ ✶  `/runmute user group`*:* Remote un-mute
+ ✶  `/ginfo username/link/ID`*:* Pulls info panel for entire group
 
 """
 
