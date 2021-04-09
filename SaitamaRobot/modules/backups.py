61e7223324738331921defdf2cb4e9a10286e099
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-10 02:31:32 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -367,18 +367,6 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
-
-__help__ = """
-*Only for group owner:*
-
- • `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
- Note that files / photos cannot be imported due to telegram restrictions.
-
- • `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
-
-"""
-
 IMPORT_HANDLER = CommandHandler("import", import_data)
 EXPORT_HANDLER = CommandHandler("export", export_data, pass_chat_data=True)
 
