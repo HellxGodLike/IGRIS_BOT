1129c80b859dfbb53ac318a12db406a1302a42e7
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-10 02:45:34 +0530

    Update dbcleanup.py

--- SaitamaRobot/modules/dbcleanup.py
+++ SaitamaRobot/modules/dbcleanup.py
@@ -145,5 +145,4 @@ BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern='db_.*')
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "DB Cleanup"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
