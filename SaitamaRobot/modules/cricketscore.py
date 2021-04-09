35f06ba9013820497865c2defb55b6e9bdd1f891
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-10 03:02:07 +0530

    Update cricketscore.py

--- SaitamaRobot/modules/cricketscore.py
+++ SaitamaRobot/modules/cricketscore.py
@@ -1,10 +1,11 @@
-
+# credits  @chsaiujwal @daisyxrobot
+# ported to masha @Mr_Dark_Prince
 
 import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from Saitama import telethn as tbot
+from SaitamaRobot import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
