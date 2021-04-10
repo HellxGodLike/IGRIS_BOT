80f84f6bce1a0ae99fa4cbae4557f3c11fdaf01b
Solo-Dragon <77414846+Solo-Dragon@users.noreply.github.com>
2021-04-10 23:03:57 +0530

    Update aifunction.py

--- SaitamaRobot/modules/aifunction.py
+++ SaitamaRobot/modules/aifunction.py
@@ -10,8 +10,7 @@ import asyncio, os
 @register(pattern="Igris (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
-   test = test.replace('Jessica', 'Igris')
-    test = test.replace('Jessica', 'Igris')
+   
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
   k = f"({r})"
   new_string = k.replace("(", "{")
@@ -53,35 +52,4 @@ async def hmm(event):
            print(e)
         
         
-    test = test.replace('Jessica', 'Igris')
-    test = test.replace('Jessica', 'Igris')
-  r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
-  k = f"({r})"
-  new_string = k.replace("(", "{")
-  lol = new_string.replace(")","}")
-  payload = lol
-  headers = {
-      'content-type': "application/json",
-      'x-forwarded-for': "<user's ip>",
-      'x-rapidapi-key': "33b8b1a671msh1c579ad878d8881p173811jsn6e5d3337e4fc",
-      'x-rapidapi-host': "iamai.p.rapidapi.com"
-      }
- 
-  response = requests.request("POST", url, data=payload, headers=headers)
-  lodu = response.json()
-  result = (lodu['message']['text'])
-  pro = result
-  pro = pro.replace('Thergiakis Eftichios','Necromander Hellx')
-  pro = pro.replace('Jessica','Igris')
-  if "Out of all ninja turtle" in result:
-   pro = "Sorry! looks I missed that. I'm at your service ask anthing sir?"
-  if "ann" in result:
-   pro = "My name is Igris"
-  if not "en" in lan and not lan == "":
-    pro = translator.translate(pro, lang_tgt=lan[0])
-  try:
-    await daisyx.send_chat_action(message.chat.id, "typing")
-    await message.reply_text(pro)
-  except CFError as e:
-         print(e)
-
+    
