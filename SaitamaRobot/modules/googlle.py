4aea4765003b3fe3d5efc6c112f567cf68626d11
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-11 00:24:34 +0530

    Update googlle.py

--- SaitamaRobot/modules/googlle.py
+++ SaitamaRobot/modules/googlle.py
@@ -90,7 +90,7 @@ useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) 
 opener.addheaders = [("User-agent", useragent)]
 
 
-@register(pattern=r"^/reverse(?: |$)(\d*)")
+@register(pattern=r"^/grs(?: |$)(\d*)")
 async def okgoogle(img):
     """ For .reverse command, Google search images and stickers. """
     if os.path.isfile("okgoogle.png"):
