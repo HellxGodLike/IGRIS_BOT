665cd5e76fce54148274f29f9d9f4c60ad681690
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-11 11:58:36 +0530

    Add files via upload

---
+++ SaitamaRobot/utils/filter_groups.py
@@ -0,0 +1,8 @@
+chat_filters_group = 1
+chatbot_group = 2
+karma_positive_group = 3
+karma_negative_group = 4
+regex_group = 5
+global_stats_group = 6
+welcome_captcha_group = 7
+antiflood_group = 8
