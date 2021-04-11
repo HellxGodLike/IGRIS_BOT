483f6b4d7bad129382381e056e80c5868455b2c6
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-11 16:05:06 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -11,10 +11,10 @@ from Python_ARQ import ARQ
 from telethon import TelegramClient
 from pyrogram import Client, errors
 
-StartTime = time.time()
-mongo_client = MongoClient
+mongo_client = MongoClient(MONGO_DB_URI)
 db = mongo_client.SaitamaRobot
 
+StartTime = time.time()
 # enable logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
