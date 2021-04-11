e0c59ded51ef60fdf01ada95d2598bba4d636143
IronMan <73984830+Uday0011@users.noreply.github.com>
2021-04-11 22:57:34 +0530

    Update filter_groups.py

--- SaitamaRobot/utils/filter_groups.py
+++ SaitamaRobot/utils/filter_groups.py
@@ -1,8 +1,3 @@
-chat_filters_group = 1
-chatbot_group = 2
+
 karma_positive_group = 3
 karma_negative_group = 4
-regex_group = 5
-global_stats_group = 6
-welcome_captcha_group = 7
-antiflood_group = 8
