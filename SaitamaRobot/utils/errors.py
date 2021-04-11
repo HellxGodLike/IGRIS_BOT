e694aa766fccd48f6963d1dbcea27c267b30ade4
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-11 23:15:19 +0530

    Update errors.py

--- SaitamaRobot/utils/errors.py
+++ SaitamaRobot/utils/errors.py
@@ -30,7 +30,7 @@ def capture_err(func):
         try:
             return await func(client, message, *args, **kwargs)
         except ChatWriteForbidden:
-            await app.leave_chat(message.chat.id)
+            await pgram.leave_chat(message.chat.id)
             return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -46,8 +46,8 @@ def capture_err(func):
                 ),
             )
             for x in error_feedback:
-                await app.send_message(
-                    LOG_GROUP_ID,
+                await pgram.send_message(
+                    EVENT_LOGS,
                     x
                 )
             raise err
