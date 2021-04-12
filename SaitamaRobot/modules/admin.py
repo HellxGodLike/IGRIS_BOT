505f9ec750fb3ab4ed56e5a5349ff6d9ac4c05c2
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-12 23:21:18 +0530

    Update admin.py

--- SaitamaRobot/modules/admin.py
+++ SaitamaRobot/modules/admin.py
@@ -435,6 +435,26 @@ def adminlist(update, context):
         msg.edit_text(text, parse_mode=ParseMode.MARKDOWN)
     except BadRequest:  # if original message is deleted
         return
+      
+ 
+__help__="""
+
+✶ `/admins`*:* list of admins in the chat
+
+*Admins only:*
+ ✶ `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
+ ✶ `/unpin`*:* unpins the currently pinned message
+ ✶ `/invitelink`*:* gets invitelink
+ ✶ `/promote`*:* promotes the user replied to
+ ✶ `/demote`*:* demotes the user replied to
+ ✶ `/title <title here>`*:* sets a custom title for an admin that the bot promoted
+ ✶ `/approval`*:* Check a user's approval status in this chat.
+ ✶ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+ ✶ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+ ✶ `/approved`*:* List all approved users.
+ ✶ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
+ 
+"""
 
  
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
@@ -456,6 +476,8 @@ dispatcher.add_handler(INVITE_HANDLER)
 dispatcher.add_handler(PROMOTE_HANDLER)
 dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
+
+
 
 __command_list__ = ["adminlist", "admins", "invitelink", "promote", "demote"]
 __handlers__ = [
