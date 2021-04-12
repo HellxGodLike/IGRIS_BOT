45a13845923378e864aa86c7df6541339c478f97
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-12 21:37:08 +0530

    Update adminhelp.py

--- SaitamaRobot/modules/helpmenu/adminhelp.py
+++ SaitamaRobot/modules/helpmenu/adminhelp.py
@@ -1,1 +1,18 @@
 
+✶ `/admins`*:* list of admins in the chat
+
+*Admins only:*
+ ✶ `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
+ ✶ `/unpin`*:* unpins the currently pinned message
+ ✶ `/invitelink`*:* gets invitelink
+ ✶ `/promote`*:* promotes the user replied to
+ ✶ `/demote`*:* demotes the user replied to
+ ✶ `/title <title here>`*:* sets a custom title for an admin that the bot promoted
+ ✶  `/approval`*:* Check a user's approval status in this chat.
+ ✶ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+ ✶ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+ ✶ `/approved`*:* List all approved users.
+ ✶ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
+ 
+
+"""
