6144672d746da73f18bf4e70a812c84e1e7841c2
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-12 21:36:00 +0530

    Update admin.py

--- SaitamaRobot/modules/admin.py
+++ SaitamaRobot/modules/admin.py
@@ -436,35 +436,7 @@ def adminlist(update, context):
     except BadRequest:  # if original message is deleted
         return
 
-
-__help__ = """
- ✶ `/admins`*:* list of admins in the chat
-
-*Admins only:*
- ✶ `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
- ✶ `/unpin`*:* unpins the currently pinned message
- ✶ `/invitelink`*:* gets invitelink
- ✶ `/promote`*:* promotes the user replied to
- ✶ `/demote`*:* demotes the user replied to
- ✶ `/title <title here>`*:* sets a custom title for an admin that the bot promoted
- ✶  `/approval`*:* Check a user's approval status in this chat.
- ✶ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
- ✶ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
- ✶ `/approved`*:* List all approved users.
- ✶ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
  
-
- *Remote commands*
- 
- ✶  `/rban user group`*:* Remote ban
- ✶ `/runban user group`*:* Remote un-ban
- ✶  `/rpunch user group`*:* Remote punch
- ✶  `/rmute user group`*:* Remote mute
- ✶  `/runmute user group`*:* Remote un-mute
- ✶  `/ginfo username/link/ID`*:* Pulls info panel for entire group
-
-"""
-
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
 
 PIN_HANDLER = CommandHandler("pin", pin, filters=Filters.group)
@@ -485,7 +457,6 @@ dispatcher.add_handler(PROMOTE_HANDLER)
 dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 
-__mod_name__ = "Admin"
 __command_list__ = ["adminlist", "admins", "invitelink", "promote", "demote"]
 __handlers__ = [
     ADMINLIST_HANDLER, PIN_HANDLER, UNPIN_HANDLER, INVITE_HANDLER,
