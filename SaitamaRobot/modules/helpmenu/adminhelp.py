610e55b98d7ac0152edf284a93c65b2e8b5401d2
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-12 21:50:20 +0530

    Update adminhelp.py

--- SaitamaRobot/modules/helpmenu/adminhelp.py
+++ SaitamaRobot/modules/helpmenu/adminhelp.py
@@ -8,11 +8,23 @@
  ✶ `/promote`*:* promotes the user replied to
  ✶ `/demote`*:* demotes the user replied to
  ✶ `/title <title here>`*:* sets a custom title for an admin that the bot promoted
- ✶  `/approval`*:* Check a user's approval status in this chat.
+ ✶ `/approval`*:* Check a user's approval status in this chat.
  ✶ `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
  ✶ `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
  ✶ `/approved`*:* List all approved users.
  ✶ `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
+  
+*Disabling :*
+
+ ✶ `/cmds`*:* check the current status of disabled commands
+
+    *Admins only:*
+ ✶ `/enable <cmd name>`*:* enable that command
+ ✶ `/disable <cmd name>`*:* disable that command
+ ✶ `/enablemodule <module name>`*:* enable all commands in that module
+ ✶ `/disablemodule <module name>`*:* disable all commands in that module
+ ✶ `/listcmds`*:* list all possible toggleable commands
+   
  
 
 """
