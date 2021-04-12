5b0fd3df8b57d204fb75cf8117842e8e17ef2ed2
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-12 21:50:57 +0530

    Update adminhelp.py

--- SaitamaRobot/modules/helpmenu/adminhelp.py
+++ SaitamaRobot/modules/helpmenu/adminhelp.py
@@ -18,7 +18,7 @@
 
  ✶ `/cmds`*:* check the current status of disabled commands
 
-    *Admins only:*
+*Admins only:*
  ✶ `/enable <cmd name>`*:* enable that command
  ✶ `/disable <cmd name>`*:* disable that command
  ✶ `/enablemodule <module name>`*:* enable all commands in that module
