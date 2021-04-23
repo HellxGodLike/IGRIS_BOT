6fbc0735fa6eb15b607bc6ccb8fb1ae69be4257c
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-23 15:30:46 +0530

    Update chatBois.py

--- SaitamaRobot/modules/chatBois.py
+++ SaitamaRobot/modules/chatBois.py
@@ -1,4 +1,4 @@
-from SaitamaRobot import app, arq, BOT_ID, DRAGONS
+from SaitamaRobot import pgram, arq, BOT_ID, DRAGONS
 from SaitamaRobot.utils.errors import capture_err
 from SaitamaRobot.utils.filter_groups import chatbot_group
 from pyrogram import filters
