1ac5901697046f0a5c9de2170142fae7e18069d9
HeLLxGodLike <ysonu4951@gmail.com>
2021-05-25 21:05:38 +0530

    Create __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -97,6 +97,7 @@ if ENV:
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
     BOT_ID = 1412878118
+    ARQ_API = "KTUCIP-UCNNCM-ADGVHH-VSUKHE-ARQ"
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())
